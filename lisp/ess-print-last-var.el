9e814af0a1500310351497a599dce1ddee36db73
David Pritchard <dpritch@live.unc.edu>
2019-04-14 22:06:22 -0400

    create dp-r-last-var-transf fcn

--- lisp/ess-print-last-var.el
+++ lisp/ess-print-last-var.el
@@ -1,7 +1,5 @@
-(defun dp-ess-find-last-command ()
-
-  (interactive)  ;; TODO: make non-interactive
-
+(defun dp-r-find-last-command ()
+  "Move point to the beginning of the last command."
   (let ((end-of-line-position))
     (goto-char (point-max))
     (move-beginning-of-line nil)
@@ -12,26 +10,46 @@
                                                (point)))
     (while (re-search-forward "[:blank:]*>" end-of-line-position t))
     ;; move point to the start of the next word
-    (re-search-forward "[^[:blank:]]" end-of-line-position)
-    (backward-char)))
+    (skip-chars-forward "[[:blank:]]" end-of-line-position)
+    ))
 
 
-(defun dp-ess-find-var-assign-after-point ()
+(defun dp-r-find-var-assign-after-point ()
   "Obtain the variable name assigned a value on the current line.
 
 TODO: this is a very rough implementation."
-  (interactive)
-
   (let ((start-pos (point))
         (after-pos))
-
-    (re-search-forward "[[:blank:]]")
+    (re-search-forward "[[:blank:]]\\|$")
+    (skip-chars-backward "[[:blank:]]" start-pos)
     (setq after-pos (point))
     (message "%s" (buffer-substring-no-properties start-pos after-pos))
     (buffer-substring-no-properties start-pos after-pos)))
 
 
-(defun testme ()
+(defun dp-r-last-var-plain ()
+  "Insert \"var\", where var is the last assigned variable."
   (interactive)
-  (dp-ess-find-last-command)
-  (dp-ess-find-var-assign-after-point))
+  (dp-r-last-var-transf "" ""))
+
+
+(defun dp-r-last-var-print ()
+  "Insert \"print(var)\", where var is the last assigned variable."
+  (interactive)
+  (dp-r-last-var-transf "print(" ")"))
+
+
+(defun dp-r-last-var-str ()
+  "Insert \"str(var)\", where var is the last assigned variable."
+  (interactive)
+  (dp-r-last-var-transf "str(" ")"))
+
+
+(defun dp-r-last-var-transf (prefix suffix)
+  "Concatenate PREFIX, the last assigned variable, and SUFFIX."
+  (interactive "sprefix text:  \nssuffix text:  ")
+  (let ((var-name))
+    (dp-r-find-last-command)
+    (setq var-name (dp-r-find-var-assign-after-point))
+    (goto-char (point-max))
+    (insert prefix var-name suffix)))
