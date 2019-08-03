e6a700757a0a7ed73f8acd6fbd6cdd97bec74c67
David Pritchard <deanpritch333@yahoo.com>
2019-08-03 13:34:25 -0400

    add binding for dp-title-case-region-or-line

---
+++ lisp/title-case.el
@@ -0,0 +1,46 @@
+;; adapted from `http://ergoemacs.org/emacs/elisp_title_case_text.html' (version
+;; 2017-01-11)
+
+(defun dp-title-case-region-or-line (@begin @end)
+  "Title case text between nearest brackets, or current line, or text selection.
+
+When called in a elisp program, *begin *end are region boundaries."
+  (interactive
+   (if (use-region-p)
+       (list (region-beginning) (region-end))
+     (let (
+           $p1
+           $p2
+           ($skipChars "^\"<>(){}[]“”‘’‹›«»「」『』【】〖〗《》〈〉〔〕"))
+       (progn
+         (skip-chars-backward $skipChars (line-beginning-position))
+         (setq $p1 (point))
+         (skip-chars-forward $skipChars (line-end-position))
+         (setq $p2 (point)))
+       (list $p1 $p2))))
+  (let* (
+         ($strPairs [
+                     [" A " " a "]
+                     [" And " " and "]
+                     [" At " " at "]
+                     [" By " " by "]
+                     [" For " " for "]
+                     [" Of " " of "]
+                     [" Or " " or "]
+                     [" On " " on "]
+                     [" The " " the "]
+                     [" To " " to "]
+                     [" Vs " " vs "]
+                     ]))
+    (save-excursion
+      (save-restriction
+        (narrow-to-region @begin @end)
+        (upcase-initials-region (point-min) (point-max))
+        (let ((case-fold-search nil))
+          (mapc
+           (lambda ($x)
+             (goto-char (point-min))
+             (while
+                 (search-forward (aref $x 0) nil t)
+               (replace-match (aref $x 1) "FIXEDCASE" "LITERAL")))
+           $strPairs))))))
