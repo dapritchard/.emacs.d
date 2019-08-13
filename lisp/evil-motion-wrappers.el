42f0c3b99fa8ce217d4f3964f623f1aa005a14b9
David Pritchard <deanpritch333@yahoo.com>
2019-08-12 23:41:16 -0400

    add insert state motion wrappers

--- lisp/evil-motion-wrappers.el
+++ lisp/evil-motion-wrappers.el
@@ -2,14 +2,14 @@
 ;; To create a file, visit it with <open> and enter text in its buffer.
 
 (defun dp-evil-normal-state-then-ace-window (arg)
-  "Wrapper for ace-window that first changes the evil state to normal state."
+  "Wrapper for `ace-window' that first changes the evil state to normal state."
   (interactive "p")
   (evil-normal-state t)
   (ace-window arg))
 
 
 (defun dp-evil-normal-state-then-avy-goto-char-2 (char1 char2 &optional arg beg end)
-  "Wrapper for avy-goto-char-2 that first changes the evil state to normal state."
+  "Wrapper for `avy-goto-char-2' that first changes the evil state to normal state."
   (interactive (list (read-char "char 1: " t)
                      (read-char "char 2: " t)
                      current-prefix-arg
